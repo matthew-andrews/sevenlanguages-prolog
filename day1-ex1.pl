d0b32025512bf1966d5d23cc10cc20d57ed3a129
Matthew Andrews <matthew@andrews.eu.com>
2013-08-19 23:03:00 +0100

    Solution 1

---
+++ day1-ex1.pl
@@ -0,0 +1,11 @@
+% Prolog - day 1, ex 1 (yep just used the books that happened to be on the table..)
+
+book('Seven Languages in Seven Weeks', 'Tate').
+book('Practical Vim', 'Neil').
+book('A Fine Balance', 'Rohinton Mistry').
+book('Such A Long Journey', 'Rohinton Mistry').
+
+% Commands to run:
+% $ swipl
+% ?- ['day-ex1'].
+% ?- book(What, 'Rohinton Mistry').
