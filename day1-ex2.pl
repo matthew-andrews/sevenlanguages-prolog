897b3a3dde5322ef0d8d08c6ff16aa031fc8dfa6
Matthew Andrews <matthew@andrews.eu.com>
2013-08-19 23:11:41 +0100

    Must be more to this task than this...

---
+++ day1-ex2.pl
@@ -0,0 +1,6 @@
+% Prolog - day 1, ex 1 (yep just used the books that happened to be on the table..)
+
+musician('Jimmy Page', 'Guitar', 'Rock').
+musician('Bill Wyman', 'Bass', 'Rock').
+musician('Christopher Wolstenholme', 'Bass', 'Rock').
+musician('Romy Madley Croft', 'Guitar', 'Electronic').
